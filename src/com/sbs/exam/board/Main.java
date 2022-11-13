898dca703dc6fce04e61a9f304a14ac9c54bd430
SangWon7242 <gotkdqja@gmail.com>
2022-11-13 13:40:28 +0900

    C:/Program Files/Git/usr/article/detail 명령어로 최근 게시물 보기

--- src/com/sbs/exam/board/Main.java
+++ src/com/sbs/exam/board/Main.java
@@ -2,19 +2,26 @@ package com.sbs.exam.board;
 
 import java.util.Scanner;
 
+/*
+### TODO 예시
+- [] /usr/article/detail 입력처리
+- [] /usr/article/detail 입력되면 가장 최근 게시물 정보 노출
+ */
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
 
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
 
-    while (true) {
+    while(true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
       if(cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
         break;
       }
       else if(cmd.equals("/usr/article/write")) {
@@ -28,17 +35,29 @@ public class Main {
         articleLastId = id;
 
         Article article = new Article(id, title, body);
-
+        lastArticle = article;
         System.out.println("생성된 게시물 객체 : " + article);
 
-        System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+        System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+      }
+      else if(cmd.equals("/usr/article/detail")) {
+        if (lastArticle == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = lastArticle;
+
+        System.out.println("== 게시물 상세내용 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
       }
       else {
-        System.out.printf("입력된 명령어 : %s\n", cmd);
+        System.out.printf("받은 명령어 : %s\n", cmd);
       }
     }
 
-    System.out.println("== 프로그램 종료 ==");
     sc.close();
   }
 }
@@ -48,7 +67,7 @@ class Article {
   String title;
   String body;
 
-  Article(int id, String title, String body) {
+  public Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
@@ -56,6 +75,9 @@ class Article {
 
   @Override
   public String toString() {
-    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+    String title = this.title != null ? "\"" + this.title + "\"" : null;
+    String body = this.body != null ? "\"" + this.body + "\"" : null;
+
+    return String.format("{id : %d, title : %s, body : %s}", id, title, body);
   }
 }
